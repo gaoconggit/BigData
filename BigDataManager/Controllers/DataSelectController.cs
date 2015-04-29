6da1cca164e393626a06e16690c3e734bec6ed8a
gaoconggit <718497701@qq.com>
2015-04-29 18:27:34 +0800

    2015年4月29日 18:27:23

---
+++ BigDataManager/Controllers/DataSelectController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigDataManager.Controllers
+{
+    public class DataSelectController : Controller
+    {
+        //
+        // GET: /DataSelect/
+        /// <summary>
+        /// 按区域查询
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AccordingToTheRegion ()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 按村居查询
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AccordingToTheVillage() 
+        {
+            return View();
+        }
+        /// <summary>
+        /// 按居住人口查询
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AccordingToThePopulation()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 人员详细结构化
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult PopulationDetailStructuring() 
+        {
+            return View();
+        }
+        /// <summary>
+        /// 材料详情
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult MaterialDetail()
+        {
+            return View();
+        }
+
+    }
+}
