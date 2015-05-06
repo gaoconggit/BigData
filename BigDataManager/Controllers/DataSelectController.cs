c221b3bd9e1f8ef97081965b5cf78c9562a0fdf2
gaoconggit <718497701@qq.com>
2015-05-06 19:04:44 +0800

    2015年5月6日 19:04:38

--- BigDataManager/Controllers/DataSelectController.cs
+++ BigDataManager/Controllers/DataSelectController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BigDataManager.Models;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,8 @@ namespace BigDataManager.Controllers
 {
     public class DataSelectController : Controller
     {
+      
+        jianzhengEntities jz = new jianzhengEntities();
         //
         // GET: /DataSelect/
         /// <summary>
@@ -16,15 +20,27 @@ namespace BigDataManager.Controllers
         /// <returns></returns>
         public ActionResult AccordingToTheRegion ()
         {
-            return View();
+            List<RegionModel>  /*结构化数据*/ Stru = jz.Database.SqlQuery<RegionModel>("SELECT count(*) as StructuringCount,a.CNAME as Name,a.`CODE` as Code from  dic_town as a left JOIN tpersons as b ON a.`CODE`=b.AI WHERE a.`CODE`<>0 GROUP BY a.`CODE`;").ToList();
+
+            List<RegionModel> /*非结构化数据*/ NotStru = jz.Database.SqlQuery<RegionModel>("select count(*) as NotStructuringCount from materialtakeinfo as a where SAVEPATH<>'' and TOWNCODE<>0 GROUP BY TOWNCODE;").ToList();
+            List<RegionModel> list = new List<RegionModel>();
+            for (int i = 0; i < Stru.Count; i++)
+            {
+                RegionModel r = new RegionModel { Name = Stru[i].Name, StructuringCount = Stru[i].StructuringCount, NotStructuringCount=NotStru[i].NotStructuringCount,Code=Stru[i].Code};
+                list.Add(r);
+            }
+            return View(list);
         }
         /// <summary>
         /// 按村居查询
         /// </summary>
         /// <returns></returns>
-        public ActionResult AccordingToTheVillage() 
+        public ActionResult AccordingToTheVillage(int pid=0) 
         {
-            return View();
+            
+            MySqlParameter parentid=new MySqlParameter("pid",pid);
+            List<RegionModel> list = jz.Database.SqlQuery<RegionModel>("select count(*) as StructuringCount,c as Name from tpersons WHERE ai=@pid GROUP BY ah;", parentid).ToList();
+            return View(list);
         }
         /// <summary>
         /// 按居住人口查询
---
+++ BigDataManager/Models/RegionModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigDataManager.Models
+{
+    public class RegionModel
+    {
+        /// <summary>
+        /// 镇街代码
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// 镇街名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 结构化总计
+        /// </summary>
+        public int StructuringCount { get; set; }
+        /// <summary>
+        /// 非结构化总计
+        /// </summary>
+        public int NotStructuringCount { get; set; }
+        
+    }
+}
